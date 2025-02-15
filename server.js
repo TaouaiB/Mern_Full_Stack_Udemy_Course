83fa084dbbae8f7bd936fbabfdd405c1d7f76a70
Taouai <baha.edine.taouai@hp.com>
2025-02-15 13:06:12 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({ path: 'config.env' });
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Our API V3');
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+	console.log(`App running on port ${PORT}`);
+});
