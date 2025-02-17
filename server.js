334866ac3ffc315b1487ec950620b3fcfadc23a0
Taouai <baha.edine.taouai@hp.com>
2025-02-17 23:30:04 +0100

    Category Crud

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
-dotenv.config({ path: 'config.env' });
+const morgan = require('morgan');
 
+dotenv.config({ path: 'config.env' });
+const dbConnection = require('./config/database');
+const categoryRoute = require('./routes/categoryRoute');
+
+// Connect with db
+dbConnection();
+
+// exporess app
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Our API V3');
-});
+// Middlewares
+app.use(express.json());
+
+if (process.env.Node_ENV == 'development') {
+	app.use(morgan('dev'));
+	console.log(`mode: ${process.env.Node_ENV}`);
+}
+
+// Mount Routes
+app.use('/api/v1/categories', categoryRoute);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
